be1d4ddf9e8b5eceb129f0c56b87eeacdc569cb3
67372a <117533205+67372a@users.noreply.github.com>
2024-11-22 17:15:14 -0600

    Updated and corrected ADOPT impl

--- custom_scheduler/LoraEasyCustomOptimizer/adopt.py
+++ custom_scheduler/LoraEasyCustomOptimizer/adopt.py
@@ -5,7 +5,7 @@ from pytorch_optimizer.base.exception import NoSparseGradientError
 from pytorch_optimizer.base.optimizer import BaseOptimizer
 from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS
 import math
-from .utils import copy_stochastic_, agc
+from .utils import copy_stochastic_
 
 class ADOPT(BaseOptimizer):
     r"""Modified Adam Can Converge with Any β2 with the Optimal Rate.
@@ -16,13 +16,8 @@ class ADOPT(BaseOptimizer):
     :param weight_decay: float. weight decay (L2 penalty).
     :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
     :param fixed_decay: bool. fix weight decay.
-    :param r: float. EMA factor. between 0.9 ~ 0.99 is preferred.
-    :param adanorm: bool. whether to use the AdaNorm variant.
-    :param adam_debias: bool. Only correct the denominator to avoid inflating step sizes early in training.
     :param eps: float. term added to the denominator to improve numerical stability.
-    :param clip: float. Clipping threshold for clipping (Root mean squared).
-    :param adaptive_clipping: bool. Use adaptive clipping instead of root mean squared.
-    :param adaptive_clip_eps: float. eps for adaptive gradient clipping.
+    :param clip: float. special form of clip for ADOPT, recommended and default value is 0.25.
     """
 
     def __init__(
@@ -34,9 +29,7 @@ class ADOPT(BaseOptimizer):
         weight_decouple: bool = False,
         fixed_decay: bool = False,
         eps: float = 1e-6,
-        clip: float = 0.0,
-        adaptive_clipping: bool = False,
-        adaptive_clip_eps: float = 1e-3,
+        clip: float = 0.25,
         **kwargs,
     ):
         self.validate_learning_rate(lr)
@@ -52,8 +45,6 @@ class ADOPT(BaseOptimizer):
             'fixed_decay': fixed_decay,
             'eps': eps,
             'clip': clip,
-            'adaptive_clipping': adaptive_clipping,
-            'adaptive_clip_eps': adaptive_clip_eps,
         }
 
         super().__init__(params, defaults)
@@ -112,47 +103,33 @@ class ADOPT(BaseOptimizer):
                     grad = grad.to(torch.float32)
                     p_fp32 = p.clone().to(torch.float32)
 
-                if group['clip'] > 0.0:
-                    if group['adaptive_clipping']:
-                        # Apply Adaptive Gradient Clipping (AGC)
-                        grad.copy_(agc(p_fp32, grad, group['adaptive_clip_eps'], group['clip']))
-                    else:
-                        # Clip the gradient 
-                        grad.div_((self.get_rms(grad).add_(group['eps']) / group['clip']).clamp_(min=1.0))
-
-                self.apply_weight_decay(
-                    p=p_fp32,
-                    grad=grad,
-                    lr=group['lr'],
-                    weight_decay=group['weight_decay'],
-                    weight_decouple=group['weight_decouple'],
-                    fixed_decay=group['fixed_decay'],
-                )
-
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
 
                  # unpack
                 if p.dtype in {torch.float16, torch.bfloat16}:
                     exp_avg, exp_avg_sq = exp_avg.to(torch.float32), exp_avg_sq.to(torch.float32)
 
+                if group['weight_decay'] != 0 and not group['decouple']:
+                    grad = grad.add(p_fp32, alpha=group['weight_decay'])
+
                 if group['step'] == 1:
                     exp_avg_sq.addcmul_(grad, grad.conj())
-
-                    # pack
-                    if p.dtype in {torch.float16, torch.bfloat16}:
-                        copy_stochastic_(state["exp_avg_sq"], exp_avg_sq)
-                        copy_stochastic_(p, p_fp32)
+                    copy_stochastic_(state["exp_avg_sq"], exp_avg_sq)
                     continue
 
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1.0 - beta2)
+                if group['weight_decay'] != 0 and group['decouple']:
+                    p_fp32.add_(p_fp32, alpha=-group['lr'] * group['weight_decay'])
 
-                de_nom = exp_avg_sq.sqrt().clamp_(min=group['eps'])
-                if group['step'] == 2:
-                    exp_avg.addcdiv_(grad, de_nom)
-                else:
-                    exp_avg.mul_(beta1).addcdiv_(grad, de_nom, value=1.0 - beta1)
+                denom = torch.clamp(exp_avg_sq.sqrt(), group['eps'])
+                normed_grad = grad.div(denom)
+                if group['clip'] is not None:
+                    clip = group['step']-1 **group['clip']
+                    normed_grad.clamp_(-clip, clip)
+
+                exp_avg.lerp_(normed_grad, 1 - beta1)
 
                 p_fp32.add_(exp_avg, alpha=-group['lr'])
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
 
                 # pack
                 if p.dtype in {torch.float16, torch.bfloat16}:
