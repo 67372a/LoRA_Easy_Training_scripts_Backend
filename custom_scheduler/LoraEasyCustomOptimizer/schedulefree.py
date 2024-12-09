c56a139759fec8921ef486ec3090fd0dc543e434
67372a <117533205+67372a@users.noreply.github.com>
2024-12-08 19:52:27 -0600

    Update ADOPTScheduleFree

--- custom_scheduler/LoraEasyCustomOptimizer/schedulefree.py
+++ custom_scheduler/LoraEasyCustomOptimizer/schedulefree.py
@@ -1,6 +1,6 @@
 # Source: https://github.com/facebookresearch/schedule_free/blob/main/schedulefree/wrap_schedulefree.py
 # Modified to be an actual optimizer, allowing it to wrap any optimizer and work in Kohya's
-from typing import Callable, Dict, Optional, Tuple, Union, List
+from typing import Callable, Dict, Optional, Tuple, Union, List, Literal
 
 import torch
 import math
@@ -8,7 +8,7 @@ import math
 from pytorch_optimizer.base.optimizer import BaseOptimizer
 from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS, OPTIMIZER
 from pytorch_optimizer.base.exception import NoSparseGradientError
-from .utils import copy_stochastic_
+from .utils import copy_stochastic_, NORM_TYPE, agc
 
 class ScheduleFreeWrapper(BaseOptimizer):
     r"""
@@ -227,32 +227,66 @@ class ScheduleFreeWrapper(BaseOptimizer):
 
 class ADOPTScheduleFree(BaseOptimizer):
     r"""Schedule-Free AdamW.
-
-    :param params: PARAMETERS. iterable of parameters to optimize or dicts defining parameter groups.
-    :param lr: float. learning rate.
-    :param betas: BETAS. coefficients used for computing running averages of gradient and the squared hessian trace.
-    :param weight_decay: float. weight decay (L2 penalty).
-    :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
-    :param fixed_decay: bool. fix weight decay.
-    :param r: float. use polynomial weighting in the average with power r.
-    :param weight_lr_power: float. during warmup, the weights in the average will be equal to lr raised to this power.
-        set to 0 for no weighting.
-    :param warmup_steps: int. enables a linear learning rate warmup.
-    :param ams_bound: bool. whether to use the AMSBound variant.
-    :param eps: float. term added to the denominator to improve numerical stability.
+    Arguments:
+        params (iterable):
+            Iterable of parameters to optimize or dicts defining
+            parameter groups.
+        lr (float):
+            Learning rate parameter (default 2.5e-3).
+        betas (float, float):
+            coefficients for momentum and exponential moving average squared (default: 0.9, 0.9999).
+        eps (float):
+            Term the denominator is minimally clamped to, to
+            improve numerical stability. (default: 1e-6).
+        eps2 (float):
+            Term to multiple the RMS of the grad to calculate adaptive eps. (default: 1e-2).
+        eps_floor (float):
+            Term to set a floor for the eps, to prevent NaNs. (default: None, disabling adaptive eps).
+        weight_decay (float):
+            Weight decay at y, i.e. a L2 penalty (default: 0.0).
+        weight_decouple (bool): 
+            the optimizer uses decoupled weight decay as in AdamW.
+        centralization (float):
+            Center model grad (default: 0.0).
+        adaptive_clip (float):
+            Adaptive clip value to apply to the gradient first, before any further processing or use by the optimizer. (default: 1.0).
+        adaptive_clip_eps (float):
+            The eps for adaptive gradient clipping, provides a minimum to avoid parameters 
+            not getting updating due to very small gradients being clipped excessively. (default: 1e-3).
+        adaptive_clip_type (string):
+            The type of clipping, can be unit or global. If done at the unit level can change
+            the direction of the gradient, while global only scales down the magnitude of the entire gradient proportionally.
+            Traditional adaptive clipping uses unit-wise, while this implementation also supports global.
+            Valid values: global, unit (default: global).
+        rectify: (bool)
+            Rectify variance as per RAdam - https://arxiv.org/abs/1908.03265 (Default: false)
+        bias_correction_beta2: (bool)
+            Apply bias correction to denominator of updates (adaptive LR). i.e.  (Default: false)
+        :param r: float. use polynomial weighting in the average with power r.
+        :param weight_lr_power: float. during warmup, the weights in the average will be equal to lr raised to this power.
+            set to 0 for no weighting.
+        :param warmup_steps: int. enables a linear learning rate warmup.
     """
 
     def __init__(
         self,
         params: PARAMETERS,
         lr: float = 2.5e-3,
-        betas: BETAS = (0.9, 0.999),
+        betas: BETAS = (0.9, 0.9999),
         weight_decay: float = 0.0,
+        weight_decouple: bool = False,
+        stable_weight_decay: bool = False,
         r: float = 0.0,
         weight_lr_power: float = 2.0,
         warmup_steps: int = 0,
         eps: float = 1e-6,
-        cautious: bool = True,
+        eps2: float = 1e-2,
+        eps_floor: float = None,
+        adaptive_clip: float = 1.0,
+        adaptive_clip_eps: float = 1e-3,
+        adaptive_clip_type: NORM_TYPE = 'layer',
+        rectify: bool = False,
+        bias_correction_beta2: bool = False,
         **kwargs,
     ):
         self.validate_learning_rate(lr)
@@ -260,23 +294,33 @@ class ADOPTScheduleFree(BaseOptimizer):
         self.validate_non_negative(weight_decay, 'weight_decay')
         self.validate_non_negative(eps, 'eps')
 
+        # Override zero to 1e-37, as zero and float32.tiny NaNs
+        # Using 1e-37 as 1e-38 NaNs for Flux loras
+        if eps_floor is not None and eps_floor < eps and eps_floor <= 0:
+            eps_floor = 1e-37
+
         defaults: DEFAULTS = {
             'lr': lr,
             'betas': betas,
             'weight_decay': weight_decay,
+            'weight_decouple':weight_decouple,
+            'stable_weight_decay':stable_weight_decay,
             'r': r,
             'weight_lr_power': weight_lr_power,
             'warmup_steps': warmup_steps,
             'eps': eps,
+            'eps2': eps2,
+            'eps_floor':eps_floor,
             'train_mode': True,
             'weight_sum': 0.0,
             'lr_max': -1.0,
-            'use_palm': kwargs.get('use_palm', False),
-            'cautious':cautious,
+            'adaptive_clip':adaptive_clip,
+            'adaptive_clip_eps':adaptive_clip_eps,
+            'adaptive_clip_type':adaptive_clip_type,
+            'rectify':rectify,
+            'bias_correction_beta2':bias_correction_beta2,
         }
         super().__init__(params, defaults)
-
-        self.base_lrs: List[float] = [group['lr'] for group in self.param_groups]
 
     def __str__(self) -> str:
         return 'ADOPTScheduleFree'
@@ -305,6 +349,7 @@ class ADOPTScheduleFree(BaseOptimizer):
     def reset(self):
         for group in self.param_groups:
             group['step'] = 0
+            group['exp_avg_mean_sqrt'] = 0.0
             for p in group['params']:
                 state = self.state[p]
 
@@ -323,13 +368,36 @@ class ADOPTScheduleFree(BaseOptimizer):
                 group['step'] += 1
             else:
                 group['step'] = 1
+                group['exp_avg_mean_sqrt'] = 0.0
+
+            param_size: int = 0
+            exp_avg_sq_sum: float = 0.0
 
             warmup_steps: int = group['warmup_steps']
             schedule: float = group['step'] / warmup_steps if group['step'] < warmup_steps else 1.0
 
             beta1, beta2 = group['betas']
 
-            lr: float = group['lr'] * schedule
+            beta2_t = beta2**group['step']
+            bias_correction2 = 1 - beta2_t
+
+            if group['rectify']:
+                # maximum length of the approximated SMA
+                rho_inf = 2 / (1 - beta2) - 1
+                # compute the length of the approximated SMA
+                rho_t = rho_inf - 2 * group['step'] * beta2_t / bias_correction2
+                rect = (
+                    ((rho_t - 4) * (rho_t - 2) * rho_inf / ((rho_inf - 4) * (rho_inf - 2) * rho_t)) ** 0.5
+                    if rho_t > 4.0
+                    else 0.0
+                )
+                lr: float = group['lr'] * rect
+            else:
+                lr: float = group['lr'] * schedule
+
+            if not group['bias_correction_beta2']:
+                bias_correction2 = 1.0
+
             lr_max = group['lr_max'] = max(lr, group['lr_max'])
 
             weight = (group['step'] ** group['r']) * (lr_max ** group['weight_lr_power'])
@@ -337,11 +405,15 @@ class ADOPTScheduleFree(BaseOptimizer):
 
             checkpoint: float = weight / weight_sum if weight_sum != 0.0 else 0.0
 
-            if group['use_palm']:
-                beta2: float = 1.0 - group['step'] ** -0.8
-                debias: float = (1.0 - beta2) / (1.0 - beta2 ** group['step'])
-            else:
-                debias: float = beta2
+            adaptive_y_lr: float = lr * (beta1 * (1.0 - checkpoint) - 1)
+            adopt_clip: float = (group['step']-1)**0.25
+
+            adaptive_clip = group["adaptive_clip"]
+            adaptive_clip_type = group["adaptive_clip_type"]
+            adaptive_clip_eps = group["adaptive_clip_eps"]
+            eps = group["eps"]
+            eps2 = group["eps2"]
+            eps_floor = group["eps_floor"]
 
             for p in group['params']:
                 if p.grad is None:
@@ -354,11 +426,14 @@ class ADOPTScheduleFree(BaseOptimizer):
                 p_fp32 = p
                 state = self.state[p]
 
+                if group["weight_decay"] != 0 and group['weight_decouple'] and group['stable_weight_Decay']:
+                    param_size += p.numel()                
+
                 if len(state) == 0:
                     state['z'] = p.clone()
                     state['exp_avg_sq'] = torch.zeros_like(p)
 
-                exp_avg_sq, z = state['z'], state['exp_avg_sq']
+                z, exp_avg_sq = state['z'], state['exp_avg_sq']
 
                 # unpack
                 if p.dtype in {torch.float16, torch.bfloat16}:
@@ -366,34 +441,52 @@ class ADOPTScheduleFree(BaseOptimizer):
                     z, exp_avg_sq = z.to(torch.float32), exp_avg_sq.to(torch.float32)
                     p_fp32 = p.clone().to(torch.float32)
 
+                if adaptive_clip > 0.0:
+                    # Apply Adaptive Gradient Clipping (AGC)
+                    grad.copy_(agc(p_fp32, grad, adaptive_clip_eps, adaptive_clip, norm_type=adaptive_clip_type))
+
+                if eps_floor is not None and eps_floor < eps:
+                    rms_grad = grad.pow(2).mean().sqrt_()
+                    curr_eps = max(min(eps, eps2 * rms_grad.item()), eps_floor) # Set a floor for eps to avoid NaN
+                else:
+                    curr_eps = eps
+
                 if group['step'] == 1:
                     exp_avg_sq.addcmul_(grad, grad.conj())
                 else:
-                    z = state['z']
+                    if not group['rectify'] or rho_t > 4.0:
+                        de_nom = exp_avg_sq.div(bias_correction2).sqrt_().clamp_(curr_eps)
 
-                    de_nom = exp_avg_sq.sqrt().add_(group['eps'])
-                    exp_avg_sq.mul_(debias).addcmul_(grad, grad.conj(), value=1 - debias)
+                        # Reuse grad buffer for memory efficiency
+                        update = grad.div(de_nom)
+                        update.clamp_(-adopt_clip, adopt_clip)
+                    else:
+                        # Fall back to SGD (or nothing)
+                        update = grad
 
-                    update = grad.div(de_nom)
-
-                    clip = (group['step']-1) **0.25
-                    normed_grad.clamp_(-clip, clip)
+                    exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
 
                     # Weight decay calculated at y
-                    if group["weight_decay"] != 0:
+                    if group["weight_decay"] != 0 and group['weight_decouple']:
+                        if group['stable_weight_decay'] and group['exp_avg_mean_sqrt'] > 0:
+                            swd_scaling = 1.0 / group['exp_avg_mean_sqrt']
+                        else:
+                            swd_scaling = 1.0
+
+                        p_fp32.data.mul_(1.0 - group['weight_decay'] * group['lr'] * swd_scaling)
+                    elif group["weight_decay"] != 0:
                         update.add_(p_fp32, alpha=group["weight_decay"])
 
-                    if group["cautious"]:
-                        # compute norm gradient
-                        mask = (update * z > 0).to(z.dtype)
-                        mask.div_(mask.mean().clamp_(min=1e-3))
-                    else:
-                        mask = 1.0
-
                     p_fp32.lerp_(z, weight=checkpoint)
-                    p_fp32.add_(update * mask, alpha=lr * (beta1 * (1.0 - checkpoint) - 1))
+                    p_fp32.add_(update, alpha=adaptive_y_lr)
 
-                    z.sub_(update * mask, alpha=lr)
+                    if group["weight_decay"] != 0 and group['weight_decouple'] and group['stable_weight_decay']:
+                        exp_avg_sq_sum += exp_avg_sq.div(bias_correction2).sum_()
+
+                    z.sub_(update, alpha=lr)
+
+                if group["weight_decay"] != 0 and group['weight_decouple'] and group['stable_weight_decay']:
+                    group['exp_avg_mean_sqrt'] = math.sqrt(exp_avg_sq_sum / param_size)
 
                 # pack
                 if p.dtype in {torch.float16, torch.bfloat16}:
