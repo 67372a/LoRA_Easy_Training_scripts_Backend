11e5dbb47dd6781769ed4370c93649023d3e2003
67372a <117533205+67372a@users.noreply.github.com>
2024-12-22 11:05:16 -0600

    Alphabetize, cleanup ineffective optimizers

--- custom_scheduler/LoraEasyCustomOptimizer/__init__.py
+++ custom_scheduler/LoraEasyCustomOptimizer/__init__.py
@@ -2,78 +2,67 @@
 from typing import Dict, List
 from LoraEasyCustomOptimizer.utils import OPTIMIZER
 
+from LoraEasyCustomOptimizer.adabelief import AdaBelief
+from LoraEasyCustomOptimizer.adammini import AdamMini
+from LoraEasyCustomOptimizer.adan import Adan
 from LoraEasyCustomOptimizer.ademamix import AdEMAMix
+from LoraEasyCustomOptimizer.adopt import ADOPT
 from LoraEasyCustomOptimizer.came import CAME
 from LoraEasyCustomOptimizer.compass import Compass, Compass8BitBNB, CompassPlus, CompassADOPT, CompassADOPTMARS
 from LoraEasyCustomOptimizer.farmscrop import FARMSCrop, FARMSCropV2
 from LoraEasyCustomOptimizer.fcompass import FCompass, FCompassPlus, FCompassADOPT, FCompassADOPTMARS
 from LoraEasyCustomOptimizer.fishmonger import FishMonger, FishMonger8BitBNB
-from LoraEasyCustomOptimizer.lpfadamw import LPFAdamW
-from LoraEasyCustomOptimizer.rmsprop import RMSProp, RMSPropADOPT, RMSPropADOPTMARS
-from LoraEasyCustomOptimizer.shampoo import ScalableShampoo
-from LoraEasyCustomOptimizer.soap import SOAP
-from LoraEasyCustomOptimizer.ranger21 import Ranger21
-from LoraEasyCustomOptimizer.lamb import Lamb
-from LoraEasyCustomOptimizer.adan import Adan
-from LoraEasyCustomOptimizer.sam import SAM, GSAM, WSAM, BSAM
-from LoraEasyCustomOptimizer.adopt import ADOPT
-from LoraEasyCustomOptimizer.grokfast import GrokFastAdamW
-from LoraEasyCustomOptimizer.adammini import AdamMini
-from LoraEasyCustomOptimizer.adai import Adai
-from LoraEasyCustomOptimizer.adabelief import AdaBelief
-from LoraEasyCustomOptimizer.galore import GaLore
-from LoraEasyCustomOptimizer.schedulefree import ScheduleFreeWrapper, ADOPTScheduleFree, ADOPTEMAMixScheduleFree, ADOPTNesterovScheduleFree, FADOPTScheduleFree, ADOPTMARSScheduleFree, FADOPTMARSScheduleFree
 from LoraEasyCustomOptimizer.fmarscrop import FMARSCrop, FMARSCropV2, FMARSCropV2ExMachina
+from LoraEasyCustomOptimizer.galore import GaLore
+from LoraEasyCustomOptimizer.grokfast import GrokFastAdamW
 from LoraEasyCustomOptimizer.laprop import LaProp
+from LoraEasyCustomOptimizer.lpfadamw import LPFAdamW
+from LoraEasyCustomOptimizer.ranger21 import Ranger21
+from LoraEasyCustomOptimizer.rmsprop import RMSProp, RMSPropADOPT, RMSPropADOPTMARS
+from LoraEasyCustomOptimizer.schedulefree import ScheduleFreeWrapper, ADOPTScheduleFree, ADOPTEMAMixScheduleFree, ADOPTNesterovScheduleFree, FADOPTScheduleFree, ADOPTMARSScheduleFree, FADOPTMARSScheduleFree
 from LoraEasyCustomOptimizer.sgd import SGDSaI
+from LoraEasyCustomOptimizer.shampoo import ScalableShampoo
 
 OPTIMIZER_LIST: List[OPTIMIZER] = [
+    ADOPT,
+    ADOPTEMAMixScheduleFree,
+    ADOPTMARSScheduleFree,
+    ADOPTNesterovScheduleFree,
+    ADOPTScheduleFree,
     AdEMAMix,
+    AdaBelief,
+    AdamMini,
+    Adan,
     CAME,
     Compass,
     Compass8BitBNB,
-    FARMSCrop,
-    FCompass,
-    FishMonger,
-    FishMonger8BitBNB,
-    LPFAdamW,
-    RMSProp,
-    ScalableShampoo,
-    SOAP,
-    Ranger21,
-    CompassPlus,
-    FCompassPlus,
-    Lamb,
-    Adan,
-    SAM,
-    GSAM,
-    WSAM,
-    BSAM,
-    ADOPT,
-    GrokFastAdamW,
-    AdamMini,
-    Adai,
-    AdaBelief,
-    GaLore,
-    ScheduleFreeWrapper,
-    FARMSCropV2,
-    FMARSCrop,
-    LaProp,
-    ADOPTScheduleFree,
-    ADOPTEMAMixScheduleFree,
-    ADOPTNesterovScheduleFree,
-    FADOPTScheduleFree,
-    ADOPTMARSScheduleFree,
-    FADOPTMARSScheduleFree,
     CompassADOPT,
-    RMSPropADOPT,
     CompassADOPTMARS,
-    RMSPropADOPTMARS,
-    FMARSCropV2,
-    SGDSaI,
+    CompassPlus,
+    FADOPTMARSScheduleFree,
+    FADOPTScheduleFree,
+    FARMSCrop,
+    FARMSCropV2,
+    FCompass,
     FCompassADOPT,
     FCompassADOPTMARS,
+    FCompassPlus,
+    FMARSCrop,
+    FMARSCropV2,
     FMARSCropV2ExMachina,
+    FishMonger,
+    FishMonger8BitBNB,
+    GaLore,
+    GrokFastAdamW,
+    LPFAdamW,
+    LaProp,
+    RMSProp,
+    RMSPropADOPT,
+    RMSPropADOPTMARS,
+    Ranger21,
+    SGDSaI,
+    ScalableShampoo,
+    ScheduleFreeWrapper,
 ]
 
 OPTIMIZERS: Dict[str, OPTIMIZER] = {str(f"{optimizer.__name__}".lower()): optimizer for optimizer in OPTIMIZER_LIST}
