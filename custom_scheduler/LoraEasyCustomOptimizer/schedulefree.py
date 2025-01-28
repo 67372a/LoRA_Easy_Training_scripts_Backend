eps adjustments, fix adaptive gradient clipping
- Apply float32 tiny as eps to Scaled Quadratic loss
- Fix adaptive gradient clipping in layer mode, was incorrectly amplifying gradients smaller than the parameter, not clipping ones larger
- Adjust all eps to be float32 tiny by default, or if eps value set to 0
ADOPTAOSchedule
- Adjust to apply clipping on step one

custom_scheduler/LoraEasyCustomOptimizer/schedulefree.py
@@ -2802,9 +2802,13 @@ class _ADOPTAOScheduleFreeBase(Optimizer):
         if update_strategy is not None and update_strategy not in {'unmodified','cautious','grams','both'}:
             raise ValueError("Invalid update strategy: {}".format(update_strategy))
         
-        # Override zero to 1e-30
+        # Override zero to tiny
         if eps_floor is not None and eps_floor < eps and eps_floor <= 0:
-            eps_floor = 1e-30
+            eps_floor = torch.finfo(torch.float32).tiny
+
+        # Override zero to tiny
+        if spam_clipping_eps is not None and spam_clipping_eps <= 0:
+            spam_clipping_eps = torch.finfo(torch.float32).tiny
 
         if block_size is None:
             if state_precision == 'parameter':
@@ -2891,10 +2895,10 @@ class _ADOPTAOScheduleFreeBase(Optimizer):
             group.setdefault("atan2_denom", False)
             group.setdefault("use_orthograd", False)
             group.setdefault("use_spam_clipping", False)
-            group.setdefault("spam_clipping_threshold", 50.0)
+            group.setdefault("spam_clipping_threshold", 1000.0)
             group.setdefault("spam_clipping_start_step", 10)
-            group.setdefault("spam_clipping_type", 'unit')
-            group.setdefault("spam_clipping_eps", '1e-16')
+            group.setdefault("spam_clipping_type", 'element')
+            group.setdefault("spam_clipping_eps", None)
             group.setdefault("use_spam_momentum_reset", False)
             group.setdefault("spam_momentum_reset_warmup_steps", 10)
             group.setdefault("spam_momentum_reset_interval_steps", 30)
@@ -3138,6 +3142,20 @@ class _ADOPTAOScheduleFreeBase(Optimizer):
                             grad_f32 = newton_schulz(grad_f32)
                         elif group["use_orthograd"] and p.ndim >= 1 and p.numel() >= 2:
                             grad_f32 = orthograd(p_f32, grad_f32)
+
+                        if group["spam_clipping_threshold"] != 0 and apply_spam_clipping and p.numel() >= 2 and p.ndim >= 1:
+                            grad_f32 = spam_grad_clipping(grad=grad_f32, 
+                                                          second_moment=exp_avg_sq_f32, 
+                                                          clip_threshold=group["spam_clipping_threshold"], 
+                                                          clip_type=group["spam_clipping_type"], 
+                                                          spam_clip_eps=group["spam_clipping_eps"])
+
+                        if group["adaptive_clip"] > 0 and p.numel() >= 2 and p.ndim >= 1:
+                            grad_f32 = agc(p=p_f32, 
+                                           grad=grad_f32, 
+                                           agc_clip_val=group["adaptive_clip"], 
+                                           agc_eps=group["adaptive_clip_eps"], 
+                                           norm_type=group["adaptive_clip_type"])
 
                         #Make a fp32 copy of exp_avg_sq_f32
                         exp_avg_sq_f32 = torch.zeros_like(p.float(), dtype=torch.float32).copy_(state["exp_avg_sq"].float())
@@ -3542,10 +3560,10 @@ class ADOPTAOScheduleFree(_ADOPTAOScheduleFreeBase):
         atan2_denom: bool = False,
         use_orthograd: bool = False,
         use_spam_clipping: bool = False,
-        spam_clipping_threshold: float = 50.0,
+        spam_clipping_threshold: float = 1000.0,
         spam_clipping_start_step: int = 10,
-        spam_clipping_type: CLIP_TYPE = 'unit',
-        spam_clipping_eps: float = 1e-16,
+        spam_clipping_type: CLIP_TYPE = 'element',
+        spam_clipping_eps: Optional[float] = None,
         use_spam_momentum_reset: bool = False,
         spam_momentum_reset_warmup_steps: int = 10,
         spam_momentum_reset_interval_steps: int = 30,

custom_scheduler/LoraEasyCustomOptimizer/utils.py
@@ -9,7 +9,7 @@ OPTIMIZER = Type[Optimizer]
 
 NORM_TYPE = Literal['unit','global','layer']
 
-CLIP_TYPE = Literal['unit','layer']
+CLIP_TYPE = Literal['unit','layer','element']
 
 STATE_PRECISION = Literal['parameter', 'q4bit', 'q8bit', 'qfp8']
 
@@ -76,7 +76,7 @@ def agc(p: torch.Tensor,
         grad: torch.Tensor, 
         agc_clip_val: float, 
         agc_eps: float = 1e-3, 
-        eps: float = 1e-16, 
+        eps: Optional[float] = None, 
         norm_type: NORM_TYPE = 'layer') -> torch.Tensor:
     r"""Clip gradient values in excess of the norm.
         Clip updates to be at most clipping * parameter_norm.
@@ -94,6 +94,10 @@ def agc(p: torch.Tensor,
         associated parameter.
     :param eps: float. simple stop from div by zero, as such should be as small as possible to avoid skewing clipping.
     """
+
+    if eps is None:
+        eps = torch.finfo(torch.float32).tiny
+
     if norm_type in {'global','layer'}:
         # Compute the global norm of the parameters and gradients
         p_norm = torch.norm(p).clamp_(min=agc_eps)
@@ -103,7 +107,7 @@ def agc(p: torch.Tensor,
         max_norm = p_norm * agc_clip_val
 
         # Compute the clipping coefficient
-        clip_coef = max(1, max_norm / g_norm.clamp(min=eps))
+        clip_coef = min(1, max_norm / g_norm.clamp(min=eps))
 
         # Scale the gradients holistically
         grad = grad * clip_coef
@@ -160,9 +164,9 @@ def schedule_beta_tc(t_beta: Optional[float], step: int, beta_initial: float, be
 def spam_grad_clipping(grad: torch.Tensor, 
                        second_moment: torch.Tensor, 
                        clip_threshold: float, 
-                       clip_type: CLIP_TYPE = 'unit', 
-                       spam_clip_eps: float = 1e-16) -> torch.Tensor:
-    if clip_type == 'unit':
+                       clip_type: CLIP_TYPE = 'element', 
+                       spam_clip_eps: float = 1e-37) -> torch.Tensor:
+    if clip_type in {'unit', 'element'}:
         # Calculate the clipping condition
         second_momentum_threshold = second_moment.mul(clip_threshold).add(spam_clip_eps)
         second_momentum_threshold_sqrt = torch.sqrt(second_momentum_threshold)
@@ -192,9 +196,9 @@ def spam_grad_clipping(grad: torch.Tensor,
 def spam_grad_clipping_logging(grad: torch.Tensor, 
                                second_moment: torch.Tensor, 
                                clip_threshold: float, 
-                               clip_type: str = 'unit', 
-                               spam_clip_eps: float = 1e-16) -> torch.Tensor:
-    if clip_type == 'unit':
+                               clip_type: str = 'element', 
+                               spam_clip_eps: float = 1e-37) -> torch.Tensor:
+    if clip_type in {'unit', 'element'}:
         # Calculate the clipping condition
         second_momentum_threshold = second_moment.mul(clip_threshold).add(spam_clip_eps)
         second_momentum_threshold_sqrt = torch.sqrt(second_momentum_threshold)
@@ -348,7 +352,10 @@ def newton_schulz(grad: torch.tensor, steps: int = 6, eps: float = 1e-7) -> torc
     return working_grad.to(dtype=original_type)
 
 # Implementation from: https://github.com/LucasPrietoAl/grokking-at-the-edge-of-numerical-stability/blob/main/orthograd.py
-def orthograd(param: torch.tensor, grad: torch.tensor, eps: float = 1e-30):
+def orthograd(param: torch.tensor, grad: torch.tensor, eps: Optional[float] = None):
+    if eps is None:
+        eps = torch.finfo(torch.float32).tiny
+
     w = param.view(-1)
     og_grad_shape = grad.shape
     grad = grad.view(-1)