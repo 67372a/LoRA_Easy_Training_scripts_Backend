3981a11460d5835e7e01d58ac5edfcb753d3f127
67372a <117533205+67372a@users.noreply.github.com>
2025-02-02 16:55:27 -0600

    bump deps
    torch==2.6.0
    torchvision==0.21.0
    triton==3.2.0
    prodigy-plus-schedule-free==1.9.0
    torchao==0.8.0
    xformers==0.0.29.post2

--- installer.py
+++ installer.py
@@ -80,32 +80,36 @@ def setup_accelerate(platform: str) -> None:
 
 def setup_venv(venv_pip):
     subprocess.check_call(
-        f"{venv_pip} install -U torch==2.5.1 torchvision==0.20.1 --index-url https://download.pytorch.org/whl/cu124",
+        f"{venv_pip} install -U torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu126",
         shell=PLATFORM == "linux",
     )
     if PLATFORM == "windows":
         subprocess.check_call("venv\\Scripts\\python.exe ..\\fix_torch.py")
 
         version = sys.version_info
-        if version.major == 3 and version.minor == 12:
+        if version.major == 3 and version.minor == 13:
             subprocess.check_call(
-                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post9/triton-3.1.0-cp312-cp312-win_amd64.whl"
+                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp313-cp313-win_amd64.whl"
+            )
+        elif version.major == 3 and version.minor == 12:
+            subprocess.check_call(
+                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp312-cp312-win_amd64.whl"
             )
         elif version.major == 3 and version.minor == 11:
             subprocess.check_call(
-                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post9/triton-3.1.0-cp311-cp311-win_amd64.whl"
+                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp311-cp311-win_amd64.whl"
             )
         elif version.major == 3 and version.minor == 10:
             subprocess.check_call(
-                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post9/triton-3.1.0-cp310-cp310-win_amd64.whl"
+                f"{venv_pip} install -U --no-deps --force-reinstall https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp310-cp310-win_amd64.whl"
             )
         
     subprocess.check_call(
-        f"{venv_pip} install -U --no-deps xformers==0.0.28.post3 --index-url https://download.pytorch.org/whl/cu124",
+        f"{venv_pip} install -U --no-deps xformers==0.0.29.post2 --index-url https://download.pytorch.org/whl/cu124",
         shell=PLATFORM == "linux",
     )
     subprocess.check_call(
-        f"{venv_pip} install -U --pre torchao --index-url https://download.pytorch.org/whl/nightly/cu124",
+        f"{venv_pip} install -U --no-deps torchao --index-url https://download.pytorch.org/whl/cu124",
         shell=PLATFORM == "linux",
     )
     
