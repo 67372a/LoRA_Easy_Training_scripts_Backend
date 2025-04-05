6d8df97d189d6e1039463850db250162ff54090e
67372a <117533205+67372a@users.noreply.github.com>
2025-04-04 20:23:49 -0500

    Optimizer six-step weights for newton_schulz

--- custom_scheduler/LoraEasyCustomOptimizer/utils.py
+++ custom_scheduler/LoraEasyCustomOptimizer/utils.py
@@ -555,13 +555,21 @@ def newton_schulz_(grad, steps=6, eps=1e-7):
     G_shape = grad.shape
     grad = grad.view(grad.size(0), -1)
 
-    a, b, c = (3.4445, -4.7750,  2.0315)
+    abc_list = [
+        (3955/1024, -8306/1024, 5008/1024),
+        (3735/1024, -6681/1024, 3463/1024),
+        (3799/1024, -6499/1024, 3211/1024),
+        (4019/1024, -6385/1024, 2906/1024),
+        (2677/1024, -3029/1024, 1162/1024),
+        (2172/1024, -1833/1024,  682/1024)
+    ]
+
     X = grad.to(dtype=torch.bfloat16, copy=True)
     if grad.size(0) > grad.size(1):
         X = X.T
 
     X /= X.norm().add(eps) # ensure top singular value <= 1
-    for _ in range(steps):
+    for a,b,c in abc_list:
         A = X @ X.T
         B = b * A + c * A @ A
         X = a * X + B @ X
