aecdedaaa4d5d96e1debc8bd8588054472fb4f10
67372a <117533205+67372a@users.noreply.github.com>
2026-08-02 14:30:50 -0500

    Revert "add BilatMuon optimizer"
    This reverts commit 10c905959b3d7310fea00e0837c172f3c08f7eba.

--- custom_scheduler/LoraEasyCustomOptimizer/__init__.py
+++ custom_scheduler/LoraEasyCustomOptimizer/__init__.py
@@ -65,7 +65,6 @@ from LoraEasyCustomOptimizer.amuse import AMUSE
 from LoraEasyCustomOptimizer.soda import SODA
 from LoraEasyCustomOptimizer.moda import MODA
 from LoraEasyCustomOptimizer.soda_wrapper import SODAWrapper
-from LoraEasyCustomOptimizer.bilatmuon import BilatMuon
 
 OPTIMIZER_LIST: List[OPTIMIZER] = [
     ABMOG,
@@ -91,7 +90,6 @@ OPTIMIZER_LIST: List[OPTIMIZER] = [
     Adopt_adv,
     Alice,
     BCOS,
-    BilatMuon,
     CAME,
     CASCADE,
     Compass,
